8455420446b846029bc1e6f9d9d18b87ff9a98be
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-12-02 22:06:12 -0300

    Correçao classe livro

--- Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/Livro.java
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/Livro.java
@@ -1,4 +1,28 @@
 package br.ifpe.prateleira.inteligente.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idEstante")
+    private Estante estante;
+
+    // Construtor padrão (obrigatório para Hibernate)
+    public Livro() {
+    }
+
+    // Getter e Setter para o ID
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
