0d8dc9cba81388ee3256d46b1fe934b57b1c14df
carlla15 <carllapatricia25@gmail.com>
2024-12-03 18:02:12 -0300

    Criando classe Categoria

---
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/Categoria.java
@@ -0,0 +1,23 @@
+package br.ifpe.prateleira.inteligente.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "Categoria")
+class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+}
