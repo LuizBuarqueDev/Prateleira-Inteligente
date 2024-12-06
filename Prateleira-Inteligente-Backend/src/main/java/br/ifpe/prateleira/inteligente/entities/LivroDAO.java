0654fdfa403bb9ed4560afd7326eb167815a4d0e
carlla15 <carllapatricia25@gmail.com>
2024-12-05 23:00:39 -0300

    Atualizando LivroDAO

--- Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/LivroDAO.java
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/LivroDAO.java
@@ -1,6 +1,7 @@
 package br.ifpe.prateleira.inteligente.entities;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 public class LivroDAO {
@@ -27,5 +28,38 @@ public class LivroDAO {
             return null;
         }
     }
+    public List<Livro> listarTodos() {
+        try {
+            return em.createQuery("FROM Livro", Livro.class).getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void atualizar(Livro livro) {
+        try {
+            em.getTransaction().begin();
+            em.merge(livro);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+        }
+    }
+
+    public void deletar(Long id) {
+        try {
+            em.getTransaction().begin();
+            Livro livro = em.find(Livro.class, id);
+            if (livro != null) {
+                em.remove(livro);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+        }
+    }
 
 }
+
