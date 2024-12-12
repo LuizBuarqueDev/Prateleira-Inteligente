b2a598c79a248f916575ad995f4ec94bf6b5b84e
carlla15 <carllapatricia25@gmail.com>
2024-12-12 16:23:03 -0300

    LivrosCategoriasDAO atualizando

--- Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/persistence/LivrosCategoriasDAO.java
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/persistence/LivrosCategoriasDAO.java
@@ -32,8 +32,29 @@ public class LivrosCategoriasDAO {
         }
     }
 
-    
+    public LivrosCategorias buscarPorId(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(LivrosCategorias.class, id);
+        } finally {
+            em.close();
+        }
+    }
 
-
-
+    public void remover(Long id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            LivrosCategorias lc = em.find(LivrosCategorias.class, id);
+            if (lc != null) {
+                em.remove(lc);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            System.err.println("Erro ao remover LivrosCategorias: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
 }
