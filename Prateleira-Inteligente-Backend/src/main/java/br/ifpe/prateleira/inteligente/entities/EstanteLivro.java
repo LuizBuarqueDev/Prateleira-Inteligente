62863ac610d3ccb915d72ba594b34c777cb7a794
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-13 19:29:22 -0300

    Varias correçoes nos relacionamentos

---
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/EstanteLivro.java
@@ -0,0 +1,51 @@
+package br.ifpe.prateleira.inteligente.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Estante_Livro")
+public class EstanteLivro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "idEstante")
+    private Estante estante;
+
+    @ManyToOne
+    @JoinColumn(name = "idLivro")
+    private Livro livro;
+
+    public EstanteLivro() {}
+
+    public EstanteLivro(Estante estante, Livro livro) {
+        this.estante = estante;
+        this.livro = livro;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Estante getEstante() {
+        return estante;
+    }
+
+    public void setEstante(Estante estante) {
+        this.estante = estante;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+}
