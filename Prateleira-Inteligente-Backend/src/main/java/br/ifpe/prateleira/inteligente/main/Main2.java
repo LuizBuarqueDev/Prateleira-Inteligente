3150a57162528dbed26ae4e5ad849f7512d63d30
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-16 09:55:41 -0300

    Main Luiz

--- Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/main/Main2.java
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/main/Main2.java
@@ -1,107 +1,72 @@
 package br.ifpe.prateleira.inteligente.main;
 
-import br.ifpe.prateleira.inteligente.entities.*;
-import br.ifpe.prateleira.inteligente.persistence.*;
+import br.ifpe.prateleira.inteligente.entities.Comentario;
+import br.ifpe.prateleira.inteligente.entities.Livro;
+import br.ifpe.prateleira.inteligente.entities.Usuario;
+import br.ifpe.prateleira.inteligente.persistence.ComentarioDAO;
+import br.ifpe.prateleira.inteligente.persistence.LivroDAO;
+import br.ifpe.prateleira.inteligente.persistence.UsuarioDAO;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main2 {
     public static void main(String[] args) {
-        // Criar instâncias dos DAOs
-        CategoriaDAO categoriaDAO = new CategoriaDAO();
-        LivroDAO livroDAO = new LivroDAO();
+        // Instanciar os DAOs
         UsuarioDAO usuarioDAO = new UsuarioDAO();
-        EstanteDAO estanteDAO = new EstanteDAO();
+        LivroDAO livroDAO = new LivroDAO();
         ComentarioDAO comentarioDAO = new ComentarioDAO();
-        EstanteLivroDAO estanteLivroDAO = new EstanteLivroDAO();
-        LivrosCategoriasDAO livrosCategoriasDAO = new LivrosCategoriasDAO();
 
-        // Criar categorias
-        Categoria categoria1 = new Categoria("Ficção Científica");
-        Categoria categoria2 = new Categoria("Tecnologia");
-        categoriaDAO.adicionar(categoria1);
-        categoriaDAO.adicionar(categoria2);
-
-        // Criar um usuário
-        Usuario usuario1 = new Usuario("João Silva", null, new ArrayList<>());
+        // Criar e adicionar usuários
+        Usuario usuario1 = new Usuario("João da Silva", null, new ArrayList<>());
+        Usuario usuario2 = new Usuario("Maria Oliveira", null, new ArrayList<>());
         usuarioDAO.adicionar(usuario1);
+        usuarioDAO.adicionar(usuario2);
 
-        // Criar uma estante para o usuário
-        Estante estante1 = new Estante("Estante de João");
-        estante1.setUsuario(usuario1);
-        estanteDAO.adicionar(estante1);
-        usuario1.setEstante(estante1);
-        usuarioDAO.atualizar(usuario1);
-
-        // Criar um livro
-        Livro livro1 = new Livro(
-                "Duna",
-                "Frank Herbert",
-                Date.valueOf("1965-08-01"),
-                "Um clássico da ficção científica",
-                "Chilton Books",
-                new ArrayList<>(), // estanteLivros
-                new ArrayList<>(), // livrosCategorias
-                new ArrayList<>()  // comentarios
-        );
-
-        Livro livro2 = new Livro(
-                "Clean Code",
-                "Robert C. Martin",
-                Date.valueOf("2008-08-01"),
-                "Guia para boas práticas de programação",
-                "Pearson",
-                new ArrayList<>(), // estanteLivros
-                new ArrayList<>(), // livrosCategorias
-                new ArrayList<>()  // comentarios
-        );
-
-        // Adicionar os livros ao banco
+        // Criar e adicionar livros
+        Livro livro1 = new Livro("Java Avançado", "Autor X", Date.valueOf(LocalDate.of(2022, 1, 15)), "Livro sobre Java", "Editora Y", null, null, new ArrayList<Comentario>());
+        Livro livro2 = new Livro("Spring Framework", "Autor Y", Date.valueOf(LocalDate.of(2021, 8, 10)), "Livro sobre Spring", "Editora Z", null, null, new ArrayList<Comentario>());
         livroDAO.adicionar(livro1);
         livroDAO.adicionar(livro2);
 
-        // Relacionar Livro e Categoria (LivrosCategorias)
-        LivrosCategorias rel1 = new LivrosCategorias(livro1, categoria1);
-        LivrosCategorias rel2 = new LivrosCategorias(livro2, categoria2);
-        livro1.getLivrosCategorias().add(rel1);
-        livro2.getLivrosCategorias().add(rel2);
-        livrosCategoriasDAO.adicionar(rel1);
-        livrosCategoriasDAO.adicionar(rel2);
+        // Criar e adicionar comentários
+        Comentario comentario1 = new Comentario(new java.util.Date(), livro1, "Livro muito bom!", usuario1);
+        Comentario comentario2 = new Comentario(new java.util.Date(), livro1, "Excelente para aprender Java.", usuario2);
+        Comentario comentario3 = new Comentario(new java.util.Date(), livro2, "Ótimo para desenvolvedores Spring.", usuario1);
 
-        // Relacionar Livro e Estante (EstanteLivro)
-        EstanteLivro estanteLivro1 = new EstanteLivro(estante1, livro1);
-        EstanteLivro estanteLivro2 = new EstanteLivro(estante1, livro2);
-        livro1.getEstanteLivros().add(estanteLivro1);
-        livro2.getEstanteLivros().add(estanteLivro2);
-        estanteLivroDAO.adicionar(estanteLivro1);
-        estanteLivroDAO.adicionar(estanteLivro2);
-
-        // Criar comentários
-        Comentario comentario1 = new Comentario(new java.util.Date(), livro1, "Livro incrível!", usuario1);
-        Comentario comentario2 = new Comentario(new java.util.Date(), livro2, "Ótimo para melhorar a qualidade do código!", usuario1);
-        livro1.getComentarios().add(comentario1);
-        livro2.getComentarios().add(comentario2);
         comentarioDAO.adicionar(comentario1);
         comentarioDAO.adicionar(comentario2);
+        comentarioDAO.adicionar(comentario3);
 
-        // Exibir informações para validar os relacionamentos
-        System.out.println("Livros na estante de " + usuario1.getNome() + ":");
-        for (EstanteLivro el : estanteLivroDAO.listarTodos()) {
-            System.out.println("- " + el.getLivro().getTitulo());
-        }
+        // Associar os comentários aos usuários
+        usuario1.getComentarios().add(comentario1);
+        usuario1.getComentarios().add(comentario3);
+        usuario2.getComentarios().add(comentario2);
 
-        System.out.println("Categorias do livro '" + livro1.getTitulo() + "':");
-        for (LivrosCategorias lc : livrosCategoriasDAO.listarTodos()) {
-            if (lc.getLivro().getId().equals(livro1.getId())) {
-                System.out.println("- " + lc.getCategoria().getNome());
+        // Associar os comentários aos livros
+        livro1.getComentarios().add(comentario1);
+        livro1.getComentarios().add(comentario2);
+        livro2.getComentarios().add(comentario3);
+
+        // Listar livros e seus comentários
+        List<Livro> livros = livroDAO.listarTodos();
+        System.out.println("Livros e Comentários:");
+        for (Livro livro : livros) {
+            System.out.println("Livro: " + livro.getTitulo() + " (" + livro.getAutor() + ")");
+            for (Comentario comentario : livro.getComentarios()) {
+                System.out.println("  - Comentário: " + comentario.getTexto() + " [Usuário: " + comentario.getUsuario().getNome() + "]");
             }
         }
 
-        System.out.println("Comentários do livro '" + livro1.getTitulo() + "':");
-        for (Comentario c : comentarioDAO.listarTodos()) {
-            if (c.getLivro().getId().equals(livro1.getId())) {
-                System.out.println(c.getUsuario().getNome() + ": " + c.getTexto());
+        // Listar usuários e seus comentários
+        List<Usuario> usuarios = usuarioDAO.listarTodos();
+        System.out.println("\nUsuários e Comentários:");
+        for (Usuario usuario : usuarios) {
+            System.out.println("Usuário: " + usuario.getNome());
+            for (Comentario comentario : usuario.getComentarios()) {
+                System.out.println("  - Comentário: " + comentario.getTexto() + " [Livro: " + comentario.getLivro().getTitulo() + "]");
             }
         }
     }
