a78c7701be3609720cb162c06f6d89934b113e58
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-17 14:34:02 -0300

    Adiçao do remove ao main2

--- Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/Livro.java
+++ Prateleira-Inteligente-Backend/src/main/java/br/ifpe/prateleira/inteligente/entities/Livro.java
@@ -42,6 +42,14 @@ public class Livro {
         this.comentarios = comentarios;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitulo() {
         return titulo;
     }
