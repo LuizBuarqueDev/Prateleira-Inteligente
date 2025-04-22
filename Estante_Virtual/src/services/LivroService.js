28eed70336e48892ff55b49455851a4ab470f55c
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-04-22 11:39:31 -0300

    Ajustes de integração (arquivos não enviados)

---
+++ Estante_Virtual/src/services/LivroService.js
@@ -0,0 +1,31 @@
+import api from '@/assets/js/axios';
+
+const LIVRO_ENDPOINT = '/livro';
+
+export default {
+  create(livro) {
+    return api.post(`${LIVRO_ENDPOINT}/create`, livro);
+  },
+
+  findById(id) {
+    return api.get(`${LIVRO_ENDPOINT}/${id}`);
+  },
+
+  update(id, livro) {
+    return api.put(`${LIVRO_ENDPOINT}/update/${id}`, livro);
+  },
+
+  delete(id) {
+    return api.delete(`${LIVRO_ENDPOINT}/delete/${id}`);
+  },
+
+  findAllByIds(ids) {
+    const params = new URLSearchParams();
+    ids.forEach(id => params.append('ids', id));
+    return api.get(`${LIVRO_ENDPOINT}/findByIds?${params.toString()}`);
+  },
+
+  findAll() {
+    return api.get(LIVRO_ENDPOINT);
+  }
+};
