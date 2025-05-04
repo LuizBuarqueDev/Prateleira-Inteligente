554e18a42e741a3148cfb41063b4f9361fa584e8
carlla15 <carllapatricia25@gmail.com>
2025-05-04 12:30:32 -0300

    ajustando service

--- Spring/src/main/java/com/prateleira_inteligente/services/PrateleiraService.java
+++ Spring/src/main/java/com/prateleira_inteligente/services/PrateleiraService.java
@@ -1,8 +1,10 @@
 package com.prateleira_inteligente.services;
 
+import com.prateleira_inteligente.dto.PrateleiraDTO;
 import com.prateleira_inteligente.entities.Livro;
 import com.prateleira_inteligente.entities.Usuario;
 import com.prateleira_inteligente.entities.UsuarioLivro;
+import com.prateleira_inteligente.mappers.PrateleiraMapper;
 import com.prateleira_inteligente.repositories.UsuarioLivroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,48 +21,50 @@ public class PrateleiraService {
     private final UsuarioLivroRepository usuarioLivroRepository;
     private final UsuarioService usuarioService;
     private final LivroService livroService;
+    private final PrateleiraMapper prateleiraMapper;
 
     @Transactional
-    public UsuarioLivro adicionarLivroNaPrateleira(Long idUsuario, Long idLivro, Double nota, String observacoes) {
-        Usuario usuario = usuarioService.getById(idUsuario);
-        Livro livro = livroService.getById(idLivro);
+    public PrateleiraDTO adicionarLivro(PrateleiraDTO dto) {
+        Usuario usuario = usuarioService.getById(dto.getIdUsuario());
+        Livro livro = livroService.getById(dto.getIdLivro());
 
         Optional<UsuarioLivro> existente = usuarioLivroRepository.findByUsuarioAndLivro(usuario, livro);
 
+        UsuarioLivro usuarioLivro;
         if (existente.isPresent()) {
-            UsuarioLivro usuarioLivro = existente.get();
-            usuarioLivro.setNota(nota);
-            usuarioLivro.setObservacoes(observacoes);
-            return usuarioLivroRepository.save(usuarioLivro);
+            usuarioLivro = existente.get();
+            usuarioLivro.setNota(dto.getNota());
+            usuarioLivro.setObservacoes(dto.getObservacoes());
+        } else {
+            usuarioLivro = prateleiraMapper.toEntity(dto);
+            usuarioLivro.setUsuario(usuario);
+            usuarioLivro.setLivro(livro);
         }
 
-        UsuarioLivro usuarioLivro = new UsuarioLivro();
-        usuarioLivro.setUsuario(usuario);
-        usuarioLivro.setLivro(livro);
-        usuarioLivro.setNota(nota);
-        usuarioLivro.setObservacoes(observacoes);
-
-        return usuarioLivroRepository.save(usuarioLivro);
+        return prateleiraMapper.toDTO(usuarioLivroRepository.save(usuarioLivro));
     }
 
     @Transactional
-    public void removerLivroDaPrateleira(Long idUsuario, Long idLivro) {
+    public void removerLivro(Long idUsuario, Long idLivro) {
         Usuario usuario = usuarioService.getById(idUsuario);
         Livro livro = livroService.getById(idLivro);
-
         usuarioLivroRepository.deleteByUsuarioAndLivro(usuario, livro);
     }
 
     @Transactional(readOnly = true)
-    public List<UsuarioLivro> listarLivrosNaPrateleira(Long idUsuario) {
+    public List<PrateleiraDTO> listarLivrosPorUsuario(Long idUsuario) {
         Usuario usuario = usuarioService.getById(idUsuario);
-        return usuarioLivroRepository.findByUsuario(usuario);
+        return usuarioLivroRepository.findByUsuario(usuario)
+                .stream()
+                .map(prateleiraMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public Optional<UsuarioLivro> buscarLivroNaPrateleira(Long idUsuario, Long idLivro) {
+    public Optional<PrateleiraDTO> buscarLivroNaPrateleira(Long idUsuario, Long idLivro) {
         Usuario usuario = usuarioService.getById(idUsuario);
         Livro livro = livroService.getById(idLivro);
-        return usuarioLivroRepository.findByUsuarioAndLivro(usuario, livro);
+        return usuarioLivroRepository.findByUsuarioAndLivro(usuario, livro)
+                .map(prateleiraMapper::toDTO);
     }
 }
