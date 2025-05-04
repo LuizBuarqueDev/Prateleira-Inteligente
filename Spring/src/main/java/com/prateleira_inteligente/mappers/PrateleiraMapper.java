ca6aaa093a2c5617d479c847b3c5b1be0015b5ba
carlla15 <carllapatricia25@gmail.com>
2025-05-04 12:31:02 -0300

    ajustando controller

--- Spring/src/main/java/com/prateleira_inteligente/mappers/PrateleiraMapper.java
+++ Spring/src/main/java/com/prateleira_inteligente/mappers/PrateleiraMapper.java
@@ -2,17 +2,10 @@ package com.prateleira_inteligente.mappers;
 
 import com.prateleira_inteligente.dto.PrateleiraDTO;
 import com.prateleira_inteligente.entities.UsuarioLivro;
-import com.prateleira_inteligente.services.LivroService;
-import com.prateleira_inteligente.services.UsuarioService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class PrateleiraMapper implements IMapper<UsuarioLivro, PrateleiraDTO> {
-
-    private final UsuarioService usuarioService;
-    private final LivroService livroService;
 
     @Override
     public PrateleiraDTO toDTO(UsuarioLivro usuarioLivro) {
@@ -29,8 +22,6 @@ public class PrateleiraMapper implements IMapper<UsuarioLivro, PrateleiraDTO> {
     public UsuarioLivro toEntity(PrateleiraDTO dto) {
         UsuarioLivro usuarioLivro = new UsuarioLivro();
         usuarioLivro.setId(dto.getId());
-        usuarioLivro.setUsuario(usuarioService.getById(dto.getIdUsuario()));
-        usuarioLivro.setLivro(livroService.getById(dto.getIdLivro()));
         usuarioLivro.setObservacoes(dto.getObservacoes());
         usuarioLivro.setNota(dto.getNota());
         return usuarioLivro;
